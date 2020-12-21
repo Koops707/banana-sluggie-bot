8536ba89396d03c4f7f9f56c4a531beba168c406
keeguzma <keeguzma@ucsc.edu>
2020-12-20 17:40:16 -0800

    Corrected "&hello" command

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def on_message(message):
 
     if message.content.startswith('&hello'):
         string_to_send = "Hello " + message.author.mention + " !"
-        await message.channel.send(code_formating_string(string_to_send))
+        await message.channel.send(string_to_send)
 
     elif message.content.startswith("&connect4"):
         await message.channel.send(code_formating_string("Connect 4 in progress - Stay Tuned!"))
