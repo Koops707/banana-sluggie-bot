a0502c58818685717fc1592905ea44f97a099bc0
keeguzma <keeguzma@ucsc.edu>
2020-12-20 16:20:52 -0800

    implemented front end op.gg functionality

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import discord
 import os
 
+
+def code_formating_string(in_string):
+    return "```" + in_string + "```"
+
+
 client = discord.Client()
 
 
@@ -15,11 +20,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('&hello'):
         string_to_send = "Hello " + message.author.mention + " !"
-        await message.channel.send(string_to_send)
+        await message.channel.send(code_formating_string(string_to_send))
 
-    elif message.content.startswith("$connect4"):
-        await message.channel.send("Connect 4 in progress - Stay Tuned!")
+    elif message.content.startswith("&connect4"):
+        await message.channel.send(code_formating_string("Connect 4 in progress - Stay Tuned!"))
+
+    elif message.content.startswith("&opgg"):
+        summoner_name = message.content.split()
+        if len(summoner_name) != 2:
+            await message.channel.send(code_formating_string("Invalid Format: format as \"&opgg <summoner_name>\""))
+        else:
+            # await message.channel.send(league.report_league_stats(summoner_name[1]))
+            if summoner_name[1] == "killerkrok":
+                await message.channel.send(code_formating_string("Doo Doo Player"))
+            else:
+                await message.channel.send(code_formating_string("Functionality not implemented yet"))
 
 client.run(os.environ['AUTH_TOKEN'])
