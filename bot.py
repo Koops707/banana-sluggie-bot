255d2ae4eb6025bc887d783b53a30e3f78a01974
keeguzma <keeguzma@ucsc.edu>
2021-03-09 15:20:05 -0800

    removed opgg

--- bot.py
+++ bot.py
@@ -32,16 +32,16 @@ async def on_message(message):
     elif message.content.startswith("&connect4"):
         await message.channel.send(code_formating_string("Connect 4 in progress - Stay Tuned!"))
 
-    elif message.content.startswith("&opgg"):
-        summoner_name = message.content.split()
-        if len(summoner_name) != 2:
-            await message.channel.send(code_formating_string("Invalid Format: format as \"&opgg <summoner_name>\""))
-        else:
-            # await message.channel.send(league.report_league_stats(summoner_name[1]))
-            if summoner_name[1] == "killerkrok":
-                await message.channel.send(code_formating_string("Doo Doo Player"))
-            else:
-                await message.channel.send(code_formating_string("Functionality not implemented yet"))
+    # elif message.content.startswith("&opgg"):
+    #    summoner_name = message.content.split()
+    #    if len(summoner_name) != 2:
+    #        await message.channel.send(code_formating_string("Invalid Format: format as \"&opgg <summoner_name>\""))
+    #    else:
+    #        # await message.channel.send(league.report_league_stats(summoner_name[1]))
+    #        if summoner_name[1] == "killerkrok":
+    #            await message.channel.send(code_formating_string("Doo Doo Player"))
+    #        else:
+    #            await message.channel.send(code_formating_string("Functionality not implemented yet"))
 
     elif message.content.startswith("&roll"):
         dice_text = message.content.split()
@@ -64,11 +64,11 @@ async def on_message(message):
         # if message.author.name == 'killerkrok':
         #    await message.channel.send("https://tenor.com/view/horny-jail-bonk-dog-hit-head-stop-being-horny-gif-17298755")
 
-        if message.author.name == "BrokeVegas":
-            await message.channel.send("https://tenor.com/view/cringy-cringe-cringe-alert-gif-17398154")
+        # if message.author.name == "BrokeVegas":
+        #    await message.channel.send("https://tenor.com/view/cringy-cringe-cringe-alert-gif-17398154")
 
+        # implement seperate functions later
 
-# implement seperate functions later
 
 def dice_roll_message(dice_roll, message_author):
     if dice_roll < 1 or dice_roll > 1000:
